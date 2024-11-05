a08bccce5467626fb0419ac038b25896c5609a22
amineElgaini <amineelgainisocial@gmail.com>
2024-11-05 19:46:44 +0100

    install tailwindScrollbar, set it up in index.html as well as tailwind.configue.json, remove the assets folder from src folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        'sans': ['Inter', 'sans-serif']
-      }
-    },
-  },
-  plugins: [],
-}
+import tailwindScrollbar from "tailwind-scrollbar";
 
+export default {
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["Inter", "sans-serif"],
+            },
+        },
+    },
+    plugins: [tailwindScrollbar],
+};
