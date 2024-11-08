0745dae34fded9a19ad6ab04660868fa92913485
amineElgainiddddddddd <amineelgainisocial@gmail.com>
2024-11-08 19:29:32 +0100

    add images to public/images. create skill.tsx and cardskill.tsx, fix hero text, update index.css for heading, call Skill.tsx in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import About from "./components/About";
 
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Skill from "./components/Skill";
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
             <main>
                 <Hero />
                 <About />
+                <Skill />
             </main>
         </>
     );
