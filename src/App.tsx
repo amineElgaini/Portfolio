58dd950d104ec2493da267f572b3d45e555b2cdd
amineElgainiddddddddd <amineelgainisocial@gmail.com>
2024-11-08 20:57:05 +0100

    add lenis library for smoth scroling, add reviews section

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
 import "./App.css";
-import About from "./components/About";
 
+import {ReactLenis} from 'lenis/react'
+
+import About from "./components/About";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Review from "./components/Review";
 import Skill from "./components/Skill";
 import Work from "./components/Work";
 
 function App() {
     return (
-        <>
+        <ReactLenis root>
             <Header />
             <main>
                 <Hero />
                 <About />
                 <Skill />
                 <Work />
+                <Review />
             </main>
-        </>
+        </ReactLenis>
     );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import './index.css'
+import 'lenis/dist/lenis.css'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
